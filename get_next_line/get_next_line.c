2dd822f9a271993058b13e8c4837640d7229a62f
effervescinn <32834764+effervescinn@users.noreply.github.com>
2020-12-11 20:57:08 +0300

    надо пофиксить пустые строки с переносом

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,25 +1,24 @@
 #include "get_next_line.h"
 
-char	*check_rest(char *rest, char **line)
+char	*check_rest(char **rest, char **line)
 {
 	char *p;
 
 	p = NULL;
-	if (rest)
+	if (*rest)
 	{
-		if ((p = ft_strchr(rest, '\n')))
+		if ((p = ft_strchr(*rest, '\n')))
 		{
 			*p = '\0';
-			*line = ft_strdup(rest);
+			*line = ft_strdup(*rest);
 			p++;
-			ft_strcpy(rest, p);
-			//rest += ft_strlen(rest) + 1;
+			ft_strcpy(*rest, p);
 		}
 		else
 		{
-			*line = ft_strdup(rest);
-			// free(rest);
-			// rest = NULL;
+			*line = ft_strdup(*rest);
+			//free(*rest);
+			*rest = NULL;
 		}
 	}
 	else
@@ -36,11 +35,11 @@ int	get_next_line(int fd, char **line)
 	char		buf[BUFFER_SIZE + 1];
 	int			ret;
 	char		*p;
-	//char		*tmp;
+	char		*tmp;
 
-	if (fd < 0 || BUFFER_SIZE < 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (-1);
-	p = check_rest(rest, line);
+	p = check_rest(&rest, line);
 	while (!p && (ret = read(fd, buf, BUFFER_SIZE)))
 	{
 		buf[ret] = '\0';
@@ -50,15 +49,14 @@ int	get_next_line(int fd, char **line)
 			p++;
 			rest = ft_strdup(p);
 		}
-		//tmp = *line;
-		*line = ft_strjoin(*line, buf);
-		//free(tmp);
+		tmp = *line;
+		*line = ft_strjoin(tmp, buf);
+		free(tmp);
 	}
 	if (ret < 0)
 		return (-1);
-	if (ret == 0 && **line == '\0')
+	if (ret == 0 && rest == NULL)
 	{
-		free(*line);
 		return (0);
 	}
 	return (1);
@@ -67,15 +65,12 @@ int	get_next_line(int fd, char **line)
 int main() {
 	int fd;
 	char *line;
-	//char *rest = "bb\0ccc\nddd\0";
-	// char *p; p = &(rest[3]);
-	//ft_strcpy(rest, p);
-	// rest += ft_strlen(rest) + 1;
-	// printf("%s", rest);
+
 	fd = open("test1.txt", O_RDWR);
 	while (get_next_line(fd, &line) > 0)
 	{
 		printf("%s\n", line);
 	}
+
 	return 0;
 }
